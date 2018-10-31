1cb4f7620971ddff77f277b7c336fdf421945a21
Chri743u <44064913+Chri743u@users.noreply.github.com>
2018-10-31 12:54:43 +0100

    Tilføjde en string Tilføjeden en system.out.print

--- src/Nikolaj.java
+++ src/Nikolaj.java
@@ -1,6 +1,10 @@
 public class Nikolaj
 {
 
+    private String Christian;
+    public static void main(String[]args){
+        System.out.println("Hello world");
+    }
 
     private String DetHarBareAtVirke;
 
