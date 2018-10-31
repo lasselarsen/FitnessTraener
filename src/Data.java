e235fb88b6dd9ccae5bd871ea62bda8baea426fb
Lasse <lasse_sl@me.com>
2018-10-31 12:13:36 +0100

    Initial commit

---
+++ src/Data.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Data
+{
+    private ArrayList<Hold> holdListe;
+    private ArrayList<Traener> traenere;
+
+    // Her oprettes en konstruktor
+    public Data()
+    {
+        this.holdListe = holdListe;
+        this.traenere = traenere;
+    }
+
+    //Her oprettes eksisterende traenere i systemet
+    private void generateData()
+    {
+        Traener henning = new Traener("Henning Andersen", "h.a@gmail.com", 1112700697, "Stengade 13",
+                                      33334444, "H.I.T.", "Fysioterapeut", 12, 10, 1234);
+        Traener anette = new Traener( "Anette Simonsen", "a.s@gmail.com", 2110850686, "Stengade 15",
+                                      44443333, "Stram op", "Personlig Traener", 5, 12, 1111);
+        Traener brian = new Traener(  "Brian Jensen", "b.j@gmail.com", 2108900687, "Husgade 15",
+                                      88888888, "Spinning", "Personlig Traener", 5, 20, 2222);
+        Traener susanne = new Traener("Susanne Nielsen", "s.n@gmail.com", 1706850896, "Murvej 5",
+                                      12345678, "Crossfit", "Crossfit level one", 6, 32, 3333);
+        Traener lasse = new Traener(  "Lasse Larsen", "l.l@gmail.com", 1903970497, "Danshojvej 1",
+                                      22909816, "Crossfit", "Crossfit level two", 2, 37, 4444);
+        Traener nikolaj = new Traener("Nikolaj Olsen", "n.o@gmail.com", 1310970897, "Broendkaervej 15",
+                                      20463633, "H.I.T.", "Fysioterapeut", 3, 38, 8888);
+        Traener christi = new Traener("Christian Adrian", "c.a@gmail.com", 1208970887, "Hyldebjergallé 50",
+                                      24812080, "Stram op", "Personlig Traener", 4, 38, 7777);
+        Traener nadia = new Traener(  "Nadia Nielsen", "n.n@gmail.com", 1808980896, "Limfjordsvej 43",
+                                      61602630, "Spinning", "Tidl. cykelrytter", 2, 39.5, 9999);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+    // Set og get metode
+    public ArrayList<Traener> getTraenere() {return traenere;}
+    public void setTraenere(ArrayList<Traener> traenere) {this.traenere = traenere;}
+}
---
+++ src/Traener.java
@@ -0,0 +1,67 @@
+// Lavet af Nadia og Nikolaj
+
+public class Traener
+{
+    private String navn;
+    private String mail;
+    private int cpr;
+    private String adresse;
+    private int mobil;
+    protected String hold;
+    private String udd;
+    private int erfaring;
+    private double timer;
+    private int pinkode;
+
+    //Her oprettes en constructor
+    public Traener(String navn, String mail, int cpr, String adresse, int mobil, String hold, String udd, int erfaring, double timer, int pinkode)
+    {
+        this.navn = navn;
+        this.mail = mail;
+        this.cpr=cpr;
+        this.adresse = adresse;
+        this.mobil = mobil;
+        this.hold = hold;
+        this.udd = udd;
+        this.erfaring = erfaring;
+        this.timer = timer;
+        this.pinkode = pinkode;
+    }
+
+    //Der oprettes get og set metoder
+    public String getNavn() {return navn;}
+    public void setNavn(String navn) {this.navn = navn;}
+
+    public String getMail() {return mail;}
+    public void setMail(String mail) {this.mail = mail;}
+
+    public int getCpr() {return cpr;}
+    public void setCpr(int cpr) {this.cpr = cpr;}
+
+    public String getAdresse() {return adresse;}
+    public void setAdresse(String adresse) {this.adresse = adresse;}
+
+    public int getMobil() {return mobil;}
+    public void setMobil(int mobil) {this.mobil = mobil;}
+
+    public String getHold() {return hold;}
+    public void setHold(String hold) {this.hold = hold;}
+
+    public String getUdd() {return udd;}
+    public void setUdd(String udd) {this.udd = udd;}
+
+    public int getErfaring() {return erfaring;}
+    public void setErfaring(int erfaring) {this.erfaring = erfaring;}
+
+    public double getTimer() {return timer;}
+    public void setTimer(double timer) {this.timer = timer;}
+
+    public int getPinkode() {return pinkode;}
+    public void setPinkode(int pinkode) {this.pinkode = pinkode;}
+
+    //Metoder
+    public void addTraener()
+    {
+        ...
+    }
+}
