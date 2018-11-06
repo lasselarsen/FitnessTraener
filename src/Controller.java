96bebce1dbbbbb9edb193b22ffec96d0ca92e813
nadiadejgaard <nadia.dejgaard@hotmail.dk>
2018-11-06 13:50:47 +0100

    Lasse og jeg har ændret rigtig meget

--- src/Controller.java
+++ src/Controller.java
@@ -123,21 +123,12 @@ public class Controller
                         switch (choice)
                         {
                             case 1:
-                                listOfBooks();
+                                AendreOplysninger();
                                 break;
                             case 2:
-                                addBook();
+                                visTraenere();
                                 break;
                             case 3:
-                                deleteBook();
-                                break;
-                            case 4:
-                                sellBook();
-                                break;
-                            case 5:
-                                buyBook();
-                                break;
-                            case 6:
                                 logOut();
                                 break;
                             default:
@@ -153,37 +144,19 @@ public class Controller
 
     private boolean userExists(int cpr, int pinkode) {
 
-        for (Traener traener : db.getTraenere()) { // for each Student object (called student) in the list of getStudents() from our database instantiated as db
-            if (traener.getCpr() == cpr && traener.getPinkode() == pinkode) {
+        for (Traener traener : db.getTraenere())
+        {
+            if (traener.getCpr() == cpr && traener.getPinkode() == pinkode)
+            {
                 this.nuTraenere = traener;
                 return true;
-
-                /* Comments to: this.currentStudent = student; (as the name of the student objects are not equal in this case, you don't necessarily need "this.". However, using "this." makes it clear that you refer to a global variable.
-
-                   Global variables ==> variables that are declared outside a function making it possible to access (use) the variable anywhere in the program.
-                   Can be used in the class just by invoking it - but also in other classes by defining a getter to get the private attribute as from the model classes.
-
-                   Local variables ==> variables that are declared inside a function and can be used only inside that function.
-
-                   Assigning the global variable to the local variable (in this case in line 78s: the global Student object equal to the local Student object found from the for loop,
-                   makes it possible to access (use) the local variable in the rest of the program (as long as you invoke the method assigning the global variable to the local variable at first,
-                   otherwise a NullPointerException will be thrown. This is the reason we validate "if (userExists)" before invoking currentStudent.getUserName() method).
-                   Without doing so, currentStudent will remain as null (without values for userName, password etc.) and can only be used for checking whether currentStudent is null or not (e.g. for login).
-
-                   ____________________________
-
-                   This is exactly the same case as when you assign your global variables to your local variables in your model class setters and set constructors.
-                   E.g. this.title = title;
-
-                   "this.title" is used to refer to the global variable title (to be used anywhere in the program) - assigning it to the local variable with the same name
-                   (making it possible to use the local variable anywhere in the program).
-                */
             }
         }
         return false;
     }
 
-    private void printMenu() {
+    private void printMenu()
+    {
         System.out.println("\nDu har nu følgende valgmuligheder: \n");
         System.out.println("1) Ændre dine oplysninger");
         System.out.println("2) Vis trænere kategoriseret efter holdtype");
@@ -191,93 +164,16 @@ public class Controller
 
     }
 
-    private void listOfBooks() {
+    private void visTraenere()
+    {
 
-        if (nuTraenere.getBooks().isEmpty()) { // same as: currentStudent.getBooks().size == 0.
-            System.out.println("Ingen bøger blev fundet i din bogsamling..");
-        } else { // We only want an overview of our books / a for loop printing our books - as long as we have any books in our personal list, otherwise it makes no sense.
-            int i = 1;
-
-            System.out.printf("%-10s %-40s %-30s %-20s\n", "Nr.", "Titel:", "Forfatter(e):", "Pris:");
-            for (Book book : currentStudent.getBooks()) {
-                System.out.printf("%-10d %-40s %-30s %-20.2f\n", i, book.getTitle(), book.getAuthor(), book.getPrice());
-                i++;
-            }
-        }
     }
 
-    private void listOfMarketPlaceBooks() {
-        int i = 1;
+    private void AendreOplysninger()
+    {
 
-        if (!db.getMarketPlace().isEmpty()) { // if list of books in the marketplace is NOT empty (!).
-            System.out.printf("%-10s %-40s %-30s %-20s\n", "Nr.", "Titel:", "Forfatter(e):", "Pris:");
-            for (Book book : db.getMarketPlace()) {
-                System.out.printf("%-10d %-40s %-30s %-20.2f\n", i, book.getTitle(), book.getAuthor(), book.getPrice());
-                i++;
-            }
-        } else {
-            System.out.println("Der er ingen brugte bøger til salg i øjeblikket.. Desværre!");
-        }
     }
 
-    private void addBook() {
-        System.out.print("\nIndtast bogens titel: ");
-        String title = scanner.nextLine();
-
-        System.out.print("\nIndtast bogens forfatter(e): ");
-        String author = scanner.nextLine();
-
-        System.out.print("\nIndtast bogens pris: ");
-        double price = scanner.nextDouble();
-
-        currentStudent.getBooks().add(new Book(title, author, price));
-        System.out.println("Bogen er nu tilføjet til din samling. Vender tilbage til hovedmenuen...");
-    }
-
-    private void deleteBook() {
-        listOfBooks();
-
-        if (!currentStudent.getBooks().isEmpty()) {
-            System.out.println("\nIndtast nummeret på bogen som skal slettes:");
-            int bookNo = scanner.nextInt();
-            Book bookToDelete = currentStudent.getBooks().get(--bookNo); // book number starts with 1, index starts with 0. We need the index of the book to sell, hence --bookNo as pre-decrement returns bookNo-1.
-            currentStudent.getBooks().remove(bookToDelete);
-            System.out.println("Bogen '" + bookToDelete.getTitle() + "' er nu slettet fra din samling. Vender tilbage til hovedmenuen...");
-        }
-    }
-
-    private void sellBook() {
-        listOfBooks();
-
-        if (!currentStudent.getBooks().isEmpty()) { // you can't sell any books if you don't have any books stored in your getBooks() arrayList.
-            System.out.println("\nIndtast nummeret på bogen som skal sælges:");
-            int bookNo = scanner.nextInt();
-            Book bookToSell = currentStudent.getBooks().get(--bookNo); // book number starts with 1, index starts with 0. We need the index of the book to sell, hence --bookNo as pre-decrement returns bookNo-1.
-
-            System.out.println("Hvilken pris vil du sælge bogen til?");
-            double sellingPrice = scanner.nextDouble();
-            bookToSell.setPrice(sellingPrice); // setting new price for the book to sell by using one of our set methods.
-
-            db.getMarketPlace().add(bookToSell); // adding the book object to the market place of books.
-            currentStudent.getBooks().remove(bookToSell); // removing the book object from currentStudent's personal list of books.
-            System.out.printf("Bogen '%s' er nu sat til salg for %.2f kroner. \n", bookToSell.getTitle(), bookToSell.getPrice());
-        }
-    }
-
-    private void buyBook() {
-        listOfMarketPlaceBooks();
-
-        if (!db.getMarketPlace().isEmpty()) { // if market place arrayList is NOT empty
-            System.out.println("\nIndtast nummeret på bogen som skal købes:");
-            int bookNo = scanner.nextInt();
-            Book bookToBuy = db.getMarketPlace().get(--bookNo);
-
-            currentStudent.getBooks().add(bookToBuy);
-            db.getMarketPlace().remove(bookToBuy);
-
-            System.out.println("Bogen er nu blevet købt.. Find den i din personlige liste med det samme!");
-        }
-    }
 
     private void logOut() {
         nuTraenere = null; // nullifying the currentStudent object resets the login args / values of the currentStudent object.
